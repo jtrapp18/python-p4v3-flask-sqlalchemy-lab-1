970560b011be79879777a2d344ba94af0133a911
+ <jacqueline.trapp18@gmail.com>
2025-01-08 20:05:26 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -22,6 +22,33 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def find_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def filter_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+
+    body = {
+        "count": len(earthquakes),
+        "quakes": [earthquake.to_dict() for earthquake in earthquakes]
+    }
+
+    status = 200
+
+    return make_response(body, status)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
